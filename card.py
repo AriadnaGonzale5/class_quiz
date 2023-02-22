0723a7cbf469fada9952956e75f1e77e0921fa4e
thecreature <121248374+AriadnaGonzale5@users.noreply.github.com>
2023-02-22 16:28:33 +0000

    Finished work for 2/22/23

--- card.py
+++ card.py
@@ -1,6 +1,25 @@
-# Name:
+# Name: Ariadna Gonzalez
 
 class Card:
-    """class representing a simple Neapolitan card. Holds a 
-    value and a suit"""
-    
+    #"""class representing a simple Neapolitan card. Holds a 
+    #value and a suit"""
+
+    def __init__(self,intvalue,suitgiven):
+        self.intvalue = intvalue
+        self.suitgiven = str(suitgiven)
+
+    def __repr__(self):
+        return f"Vector2d({self.intvalue},{self.suitgiven})"
+    def __str__(self):
+        return f"{self.intvalue} of {self.suitgiven}"
+    def __eq__(self,other):
+        if self.intvalue == other.intvalue:
+            if self.suitgiven == other.suitgiven:
+                return True
+            else:
+                return False
+        else:
+            return False
+hi = Card(2,'hearts')
+bye = Card(3,'diamonds')
+totallyhi = Card(2,'hearts')
